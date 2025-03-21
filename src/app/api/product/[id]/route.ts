81159963e9f4d63490cbd7443bad98f9e4e88f44
Alejou-geeks5g <alejandroalvarez@geek5g.com>
2025-03-21 13:15:00 -0500

    Nextjs build fixed

--- src/app/api/product/[id]/route.ts
+++ src/app/api/product/[id]/route.ts
@@ -1,111 +1,99 @@
 import Product from "@/models/Product";
 import dbConnect from "@/lib/db";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { RouteContext } from "@/utils/interfaces";	
+// DELETE
+export async function DELETE(req: NextRequest, context: RouteContext) {
+  try {
+    await dbConnect();
+    const { id } = await context.params;
 
-export async function DELETE(req: Request, {params}: {params: {id: string}}) {
-
-    try {
-        await dbConnect()
-
-        const { id } = params
-
-        if (!id) {
-            return NextResponse.json(
-                { mesaage: 'ID is required' },
-                { status: 400 }
-            )
-        }
-
-        const deleteProduct = await Product.findByIdAndDelete(id)
-        if (!deleteProduct) {
-            return NextResponse.json(
-                { mesaage: 'Product not found' },
-                { status: 400 }
-            )
-        } else {
-            return NextResponse.json(
-                { mesaage: 'Product deleted successfully' },
-                { status: 400 }
-            )
-        }
-    } catch (error) {
-        return NextResponse.json(
-            { message: 'Internal Server Error', error },
-            { status: 500 }
-        )
+    if (!id) {
+      return NextResponse.json({ message: "ID is required" }, { status: 400 });
     }
+
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    if (!deleteProduct) {
+      return NextResponse.json(
+        { message: "Product not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Product deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error", error },
+      { status: 500 }
+    );
+  }
 }
 
+// PUT
+export async function PUT(req: NextRequest, context: RouteContext) {
+  try {
+    await dbConnect();
+    const { id } = await context.params;
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
-    
-    try {
-        await dbConnect()
-
-        const { id } = params
-
-        if (!id) {
-            return NextResponse.json(
-                { mesaage: 'ID is required' },
-                { status: 400 }
-            )
-        }
-        
-        const body = await req.json()
-        const product = await Product.findById(id)
-        if (!product) {
-            return NextResponse.json(
-                { message: 'Product not found' },
-                { status: 404}
-            )
-        }
-
-        Object.assign(product, body)
-        
-        await product?.save()
-        return NextResponse.json(
-            { messaje: 'Product update successfully', product },
-            { status: 200 }
-        )
-
-    } catch (error) {
-        return NextResponse.json(
-            { message: 'Internal Server Error', error },
-            { status: 500 }
-        )
+    if (!id) {
+      return NextResponse.json({ message: "ID is required" }, { status: 400 });
     }
+
+    const body = await req.json();
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return NextResponse.json(
+        { message: "Product not found" },
+        { status: 404 }
+      );
+    }
+
+    Object.assign(product, body);
+    await product.save();
+
+    return NextResponse.json(
+      { message: "Product updated successfully", product },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error", error },
+      { status: 500 }
+    );
+  }
 }
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-    
-    try {
-        await dbConnect()
+// GET
+export async function GET(req: NextRequest, context: RouteContext) {
+  try {
+    await dbConnect();
+    const { id } = await context.params;
 
-        const { id } = params
-
-        if (!id) {
-            return NextResponse.json(
-                { mesaage: 'ID is required' },
-                { status: 400 }
-            )
-        }
-        const product = await Product.findById(id)
-        if (!product) {
-            return NextResponse.json(
-                { message: 'Product not found' },
-                { status: 404}
-            )
-        }
-
-        return NextResponse.json(
-            { messaje: 'Product get successfully', product },
-            { status: 200 }
-        )
-
-    } catch (error) {
-        return NextResponse.json(
-            { message: 'Internal Server Error', error },
-            { status: 500 }
-        )
+    if (!id) {
+      return NextResponse.json({ message: "ID is required" }, { status: 400 });
     }
+
+    const product = await Product.findById(id);
+    if (!product) {
+      return NextResponse.json(
+        { message: "Product not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Product retrieved successfully", product },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error", error },
+      { status: 500 }
+    );
+  }
 }
--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -2,123 +2,99 @@ import User from "@/models/User";
 import dbConnect from "@/lib/db";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { RouteContext } from "@/utils/interfaces";
 
+export async function DELETE(req: Request, context: RouteContext) {
+  try {
+    await dbConnect();
 
-export async function DELETE(req: Request, {params}: { params: Promise<{ id: string }> }) {
-    try {
-        await dbConnect();
+    const { id } = await context.params;
 
-        const { id } = await params;
-
-        if (!id) {
-            return NextResponse.json(
-                { message: "ID is required" },
-                { status: 400 }
-            );
-        }
-
-        const deleteUser = await User.findByIdAndDelete(id);
-        if (!deleteUser) {
-            return NextResponse.json(
-                { message: 'User not found' },
-                { status: 400 }
-            );
-        }
-
-        return NextResponse.json(
-            { message: 'User deleted successfully' },
-            { status: 200 }
-        );
-    } catch (error) {
-        return NextResponse.json(
-            { message: 'Internal Server Error', error },
-            { status: 500 }
-        );
+    if (!id) {
+      return NextResponse.json({ message: "ID is required" }, { status: 400 });
     }
+
+    const deleteUser = await User.findByIdAndDelete(id);
+    if (!deleteUser) {
+      return NextResponse.json({ message: "User not found" }, { status: 400 });
+    }
+
+    return NextResponse.json(
+      { message: "User deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error", error },
+      { status: 500 }
+    );
+  }
 }
 
-export async function GET(req: Request, { params }: { params: Promise< { id: string }> }) {
+export async function GET(req: Request, context: RouteContext) {
+  try {
+    await dbConnect();
 
-    try {
-        await dbConnect()
+    const { id } = await context.params;
 
-        const { id } = await params
-
-        if (!id) {
-            return NextResponse.json(
-                { mesaage: 'ID is required' },
-                { status: 400 }
-            )
-        }
-
-        const user = await User.findById(id)
-        if (user) {
-            return NextResponse.json(
-                {
-                    mesaage: 'User found successfully',
-                    user
-                },
-                { status: 400 }
-            )
-        } else {
-            return NextResponse.json(
-                { mesaage: 'User not found' },
-                { status: 400 }
-            )
-        }
-    } catch (error) {
-        
-        return NextResponse.json(
-            { message: 'Internal Server Error', error },
-            { status: 500 }
-        )
+    if (!id) {
+      return NextResponse.json({ mesaage: "ID is required" }, { status: 400 });
     }
+
+    const user = await User.findById(id);
+    if (user) {
+      return NextResponse.json(
+        {
+          mesaage: "User found successfully",
+          user,
+        },
+        { status: 400 }
+      );
+    } else {
+      return NextResponse.json({ mesaage: "User not found" }, { status: 400 });
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error", error },
+      { status: 500 }
+    );
+  }
 }
 
+export async function PUT(req: Request, context: RouteContext) {
+  try {
+    await dbConnect();
 
-export async function PUT(req: Request, { params }: { params: Promise<{ id: string }> }) {
-    
-    try {
-        await dbConnect()
-        
-        const { id } = await params
-        
-        if (id) {
-            return NextResponse.json(
-                { mesaage: 'ID is required' },
-                { status: 400 }
-            )
-        }
-        
-        const body = await req.json()
-        const user = await User.findById(id)
-        if (!user) {
-            return NextResponse.json(
-                { message: 'User not found' },
-                { status: 404}
-            )
-        }
+    const { id } = await context.params;
 
-        if (body.password) {
-            const samePass = bcrypt.compareSync(body.password, user.password)
-            if (!samePass) {
-                user.password = bcrypt.hashSync(body.password,10)
-            } 
-        }
-
-        Object.assign(user, body)
-        
-        await user.save()
-        return NextResponse.json(
-            { messaje: ' User update successfully', user },
-            { status: 200}
-        )
-
-    } catch (error) {
-        
-        return NextResponse.json(
-            { message: 'Internal Server Error', error },
-            { status: 500 }
-        )
+    if (id) {
+      return NextResponse.json({ mesaage: "ID is required" }, { status: 400 });
     }
+
+    const body = await req.json();
+    const user = await User.findById(id);
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    if (body.password) {
+      const samePass = bcrypt.compareSync(body.password, user.password);
+      if (!samePass) {
+        user.password = bcrypt.hashSync(body.password, 10);
+      }
+    }
+
+    Object.assign(user, body);
+
+    await user.save();
+    return NextResponse.json(
+      { messaje: " User update successfully", user },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error", error },
+      { status: 500 }
+    );
+  }
 }
